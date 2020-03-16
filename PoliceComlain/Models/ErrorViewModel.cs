b0ed8d858494f7b4f6cd19506fad377fe5d64101
Eranga Gamagedara <developer.eranga@gmail.com>
2020-03-16 13:35:52 +0530

    initial

---
+++ PoliceComlain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PoliceComlain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
